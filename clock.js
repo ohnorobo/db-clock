4c41cbfc477889c02586e04ebbb214a75ebd7c8f
ohnorobo <sar.laplante@gmail.com>
2024-01-31 17:15:39 +0100

    convert minute and hour hands to functional

--- clock.js
+++ clock.js
@@ -1,3 +1,8 @@
+// This tries to imitate the 'master timed' behavior of the DB clocks.
+// https://en.wikipedia.org/wiki/Swiss_railway_clock#Technology
+
+const bezier = require(['./bezier_easing']);
+
 // Math constants
 const circleDegrees = 360;
 
@@ -6,6 +11,7 @@ const secPerMinute = 60;
 const minPerHour = 60;
 const hoursPerClockCycle = 12;
 const msPerSec = 1000;
+const wobbleLengthSec = 1.2;
 
 // The second hand runs slightly fast
 // https://en.wikipedia.org/wiki/Swiss_railway_clock#Technology
@@ -23,38 +29,123 @@ const hoursAdditionalMinuteAngleDiff = hourAngleDiff / minPerHour
 secondUpdateInterval = (updateIntervalSeconds / secPerMinute) * msPerSec
 minuteUpdateInterval = secPerMinute * msPerSec
 
-function calculateSecondsAngle() {
+
+function cubicBezierEase(t) {
+    // Return the diff from 0 to change in a cubic bezier ease over time.
+    // t is a time from 0 to 1
+
+    const p1 = .35;
+    const p2 = .11;
+    const p3 = .18;
+    const p4 = .92;
+
+    var easing = BezierEasing(p1, p2, p3, p4);
+
+    return easing(t)
+
+    /*
+    return (1-t)^3 * p1 + 
+           3 * (1-t)^2 * p2 + 
+           3 * (1-t) * p3 + 
+           t^3 * p4;
+    */
+}
+
+
+function easeInOutCubic(x) {
+    return x < 0.5 ? 4 * x^3 : 1 - (-2 * x + 2)^3 / 2;
+}
+
+
+function A (aA1, aA2) { return 1.0 - 3.0 * aA2 + 3.0 * aA1; }
+function B (aA1, aA2) { return 3.0 * aA2 - 6.0 * aA1; }
+function C (aA1)      { return 3.0 * aA1; }
+
+// Returns x(t) given t, x1, and x2, or y(t) given t, y1, and y2.
+function calcBezier (aT, aA1, aA2) { return ((A(aA1, aA2) * aT + B(aA1, aA2)) * aT + C(aA1)) * aT; }
+
+
+
+function dampedSpring(t) {
+    // A damped spring that moves from 0 to 1 in the domain t=[0,1] and bounces n times
+
+    amplitude = 1;
+    dampening = .9; // 0-1,  0 = no dampening, 1 = infinite dampening
+    num_peaks = 7;
+
+    return -1 * amplitude * Math.pow((1 - dampening), t) * Math.cos((num_peaks + .5) * Math.PI * t) + 1
+}
+
+
+function baseSecondsAngle(now) {
     // Get current angle for the second hand out of 360 degrees
-    const now = new Date();
     var seconds = now.getSeconds();
     // The second hand runs slightly fast
     var fastSeconds = Math.round(seconds * (secPerMinute / updateIntervalSeconds));
-    return fastSeconds * oneTickAngleDiff;
+    // but pauses at the top
+    return Math.min(fastSeconds * oneTickAngleDiff, 360);
 }
 
-function calculateMinutesAngle() {
+function baseMinutesAngle(now) {
     // Get current angle for the minute hand out of 360 degrees
-    const now = new Date();
     var minutes = now.getMinutes();
     return minutes * oneTickAngleDiff;
 }
 
-function calculateHourAngle() {
+function baseHourAngle(now) {
     // Get current angle for the hour hand out of 360 degrees
-    const now = new Date();
     var hours = now.getHours();
     var minutes = now.getMinutes();
     return (hourAngleDiff * (hours % 12)) + (hoursAdditionalMinuteAngleDiff * minutes);
 }
+
+
+function calculateSecondsAngle(now) {
+    // TODO calculate ease
+    return baseSecondsAngle(now);
+}
+
+function calculateMinutesAngle(now) {
+    var minuteAngle = baseMinutesAngle(now);
+    var msPastMinuteStart = (now.getSeconds() * msPerSec + now.getMilliseconds());
+
+    if (msPastMinuteStart <= wobbleLengthSec * msPerSec) {
+        // How much of the wobble time is completed [0,1]
+        var wobble_fraction = msPastMinuteStart / (msPerSec * wobbleLengthSec);
+        // x value of wobble [0,1+] where 1 is the ending point
+        var wobble = dampedSpring(wobble_fraction);
+
+        minuteAngle = minuteAngle + oneTickAngleDiff * (wobble - 1);
+    }    
+    return minuteAngle
+}
+
+function calculateHourAngle(now) {
+    var hourAngle = baseHourAngle(now);
+    var msPastMinuteStart = (now.getSeconds() * msPerSec + now.getMilliseconds());
+
+    if (msPastMinuteStart <= wobbleLengthSec * msPerSec) {
+        // How much of the wobble time is completed [0,1]
+        var wobble_fraction = msPastMinuteStart / (msPerSec * wobbleLengthSec);
+        // x value of wobble [0,1+] where 1 is the ending point
+        var wobble = dampedSpring(wobble_fraction);
+
+        hourAngle = hourAngle + hoursAdditionalMinuteAngleDiff * (wobble - 1);
+    }    
+    return hourAngle
+}
+
 
 function setClock() {
     var secondHand = document.querySelector('#second-hand');
     var minuteHand = document.querySelector('#minute-hand');
     var hourHand = document.querySelector('#hour-hand');
 
-    var secondAngle = calculateSecondsAngle();
-    var minuteAngle = calculateMinutesAngle();
-    var hourAngle = calculateHourAngle()
+    var now = new Date();
+
+    var secondAngle = calculateSecondsAngle(now);
+    var minuteAngle = calculateMinutesAngle(now);
+    var hourAngle = calculateHourAngle(now)
 
     secondHand.style.transform = 'rotate('+ secondAngle +'deg)';
     minuteHand.style.transform = 'rotate('+ minuteAngle +'deg)';
@@ -70,39 +161,6 @@ function setHandAngleClosure(hand, angle) {
     return(function() {
         setHandAngle(hand, angle)
     })
-}
-
-function rotateElastic(hand, oldAngle, newAngle) {
-    // Rotate the hand element
-    // Over 1.2 seconds, between oldAngle and newAngle
-    // approximate the damping function
-    // y(t) = -(e^(-t)cos(pi2t)) + 1
-    // we want 8 mins + maxes, so the period for t is 4.
-    overallTime = 1.2 * msPerSec;
-
-    // See https://easings.net/#easeOutElastic
-    // Unfortunatly rotate doesn't work with css keyframes
-    // so we're stuck with this
-    var elasticMovements = [
-        // fraction of animation, fraction of motion
-        [1/8, 1.6065],
-        [2/8, 0.6321],
-        [3/8, 1.2231],
-        [4/8, 0.8646],
-        [5/8, 1.0821],
-        [6/8, 0.9502],
-        [7/8, 1.0302],
-        [1,   1],
-    ]
-
-    for (var i = 0; i < elasticMovements.length; i++) {
-        var timeFraction = elasticMovements[i][0];
-        var motionFraction = elasticMovements[i][1];
-        var time = overallTime * timeFraction;
-        var angle = ((newAngle - oldAngle) * motionFraction) + oldAngle;
-
-        setTimeout(setHandAngleClosure(hand, angle), time);
-    }
 }
 
 // Sometime for js timing reasons updateSecondHandUntilMinuteEnds
@@ -154,40 +212,6 @@ function updateSecond() {
     setHandAngle(secondHand, newSecondAngle);
 }
 
-function updateMinute() {
-    var minuteHand = document.querySelector('#minute-hand');
-    
-    // Angle of the current minute hand.
-    // This can to arbitrarily above 360 as the hand keeps rotating.
-    var oldMinuteAngle = Number(minuteHand.style.transform.match(/\d+/)[0])
-
-    // New angle the minute hand should be at out of 360
-    var newMinuteAngleMod360 = calculateMinutesAngle();
-
-    diff = (newMinuteAngleMod360 - (oldMinuteAngle % circleDegrees));
-    if (diff < 0) {diff = diff + circleDegrees;}
-    newMinuteAngle = oldMinuteAngle + diff;
-
-    rotateElastic(minuteHand, oldMinuteAngle, newMinuteAngle);
-}
-
-function updateHour() {
-    var hourHand = document.querySelector('#hour-hand');
-
-    // Angle of the current hour hand.
-    // This can to arbitrarily above 360 as the hand keeps rotating.
-    var oldHourAngle = Number(hourHand.style.transform.match(/\d+/)[0])
-
-    // New angle the hour hand should be at out of 360
-    var newHourAngleMod360 = calculateHourAngle();
-
-    diff = (newHourAngleMod360 - (oldHourAngle % circleDegrees));
-    if (diff < 0) {diff = diff + circleDegrees;}
-    newHourAngle = oldHourAngle + diff;
-
-    rotateElastic(hourHand, oldHourAngle, newHourAngle);
-}
-
 function timeUntilNextMinuteBoundary() {
     // returns milliseconds until next minute boundary
     var now = new Date();
@@ -201,32 +225,4 @@ function timeUntilNextMinuteBoundary() {
     return diff;
 }
 
-function runClock() {
-    // This tries to imitate the 'master timed' behavior of the DB clocks.
-    // https://en.wikipedia.org/wiki/Swiss_railway_clock#Technology
-    var now = new Date();
-    // In theory in every call of this except the first one ms should be near 0 or 1000 ms
-
-    updateMinute();
-    updateHour();
-    var timeUntilNextMinute = timeUntilNextMinuteBoundary();
-
-    setTimeout(runClock, timeUntilNextMinute);
-
-    updateSecondHandUntilMinuteEnds();
-}
-
-function resetClock() {
-    updateSecond();
-    updateMinute();
-    updateHour();
-    // Start second hand if it's stopped
-    updateSecondHandUntilMinuteEnds();
-}
-
-setClock();
-runClock();
-
-// js timing gets weird if the tab doesn't have focus
-// if we regain focus reset the clock to keep things from being too off
-window.onfocus = resetClock;
+setInterval(setClock, 1);
